719647e20453df6e55c4cb73c248895b4e6f1986
sensible-gateway-6049 <greedybrain18@gmail.com>
2019-09-06 19:24:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,7 +10,7 @@ def load_library(file)
   }
   emo_file.each do |key, value|
     conversion_hash[:get_meaning][value[1]] = key
-    conversion_hash[:get_emoticon][value[0]] = emo_file[key][1]
+    conversion_hash[:get_emoticon][value[0]] = emo_file[key][1] #grab val of meaning
     binding.pry
   end
   conversion_hash
