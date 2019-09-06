021a9a2d3db80b5f103d170ccf15332538e8a442
sensible-gateway-6049 <greedybrain18@gmail.com>
2019-09-06 19:08:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,24 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(file)
+  emo_file = YAML.load_file(file)
+  conversion_hash = {
+    :get_meaning => {},
+    :get_emoticon => {}
+  }
+  emo_file.each do |key, value|
+    conversion_hash[:get_meaning][value[1]] = key
+    conversion_hash[:get_emoticon][value[0]] = emo_file[key][1]
+  end
+  conversion_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(path, eng_emo)
+  load_library(path)
 end
 
-def get_english_meaning
-  # code goes here
-end
+# def get_english_meaning
+#   # code goes here
+# end
